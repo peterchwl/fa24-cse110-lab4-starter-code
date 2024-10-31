13d71431beb1b86bb6e8158dab9e1eb1af733d5a
Peter Lee <chiwoonlee@gmail.com>
2024-10-30 21:21:41 -0700

    fixed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,5 +5,5 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect("THIS IS WRONG").toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument();
 });

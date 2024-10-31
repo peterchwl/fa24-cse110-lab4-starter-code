6108d402318b0f3c191a35116fecdef1c43a3678
Peter Lee <chiwoonlee@gmail.com>
2024-10-30 21:57:12 -0700

    adls;fkjasd;

---
+++ client/src/components/Remaining.tsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+const Remaining = () => {
+  const { expenses, budget } = useContext(AppContext);
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total = total + item.cost);
+  }, 0);
+
+  const alertType = totalExpenses > budget ? "alert-danger" : "alert-success";
+
+  // Exercise: Create an alert when Remaining is less than 0.
+  if (budget - totalExpenses < 0){
+    alert("You have exceeded your budget!");
+  }
+
+  return (
+    <div className={`alert ${alertType}`}>
+      <span>Remaining: ${budget - totalExpenses}</span>
+    </div>
+  );
+};
+
+export default Remaining;
